58c195f0cd1f9cdd203f0eaec09a492627a55003
Jeurei <56633996+Jeurei@users.noreply.github.com>
2020-02-20 23:21:11 +0300

    попапы

--- js/map.js
+++ js/map.js
@@ -1,9 +1,12 @@
-let coordinates=[59.938549,30.322849]
-function initMap (ymaps) {
+let coordinates = [59.938549, 30.322849];
+function initMap(ymaps) {
   var myMap = new ymaps.Map(document.querySelector(".modal-map"), {
       center: coordinates,
       zoom: 17
-  }),
-  myPlacemark = new ymaps.Placemark(coordinates);
-  myMap.geoObjects.add(myPlacemark)
+    }),
+    myPlacemark = new ymaps.Placemark(coordinates);
+  myMap.controls.remove(`trafficControl`);
+  myMap.controls.remove(`typeSelector`);
+  myMap.controls.remove(`fullscreenControl`);
+  myMap.geoObjects.add(myPlacemark);
 }
